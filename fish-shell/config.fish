63a71e43e1e455c958a8914efd292ac51be6c5d6
William Fiset <william@panther.local>
2026-04-12 10:41:28 +0800

    Update fish config

--- fish-shell/config.fish
+++ fish-shell/config.fish
@@ -1,53 +1,26 @@
 set fish_greeting ''
 
-# Export statments
-
-# Adds '.' to $CLASSPATH if it's not already there.
-if not contains . $CLASSPATH
-  set -x CLASSPATH . $CLASSPATH
-end
-
-# Adds '.' to $PATH if it's not already there.
-if not contains . $PATH
-  set -x PATH . $PATH
-end
-
-### Abbreviations ###
+fish_add_path ~/.local/bin
 
 # Command shortcuts
+
 abbr -a l ls -F
 abbr -a ll ls -lhF
 abbr -a la ls -ahF
-
-# Git commands
-abbr -a gita git add -A
-abbr -a gits git status
-abbr -a gitc git commit -m 
-abbr -a gitp git push
-abbr -a gitl "git log --oneline -n 10"
 
 abbr -a c cd
 abbr -a .. cd ..
 abbr -a ... cd ../..
 abbr -a .... cd ../../..
 abbr -a ..... cd ../../../..
-abbr -a ...... cd ../../../../..
-abbr -a ....... cd ../../../../../..
 
 # Path shortcuts
-abbr -a kat cd ~/Dropbox/Programming/Competitions/Kattis
-abbr -a drive cd ~/Google\ Drive/
-abbr -a euler cd ~/Dropbox/Programming/Project_Euler/WIP
-abbr -a uni cd ~/Dropbox/University/
-abbr -a drop cd ~/Dropbox
+abbr -a drive cd ~/Google\ Drive/My\ Drive
 abbr -a github cd ~/Desktop/GitHub
 abbr -a desk cd ~/Desktop
-abbr -a comp cd ~/Dropbox/Programming/Competitions
 
-# Make directory and cd into it
-function mkcd 
-  mkdir -p $argv; and cd $argv
-end
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/williamfiset/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/williamfiset/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/williamfiset/Downloads/google-cloud-sdk/path.fish.inc'; end; end
+# Git commands
+abbr -a gita git add -A
+abbr -a gits git status
+abbr -a gitc git commit -m 
+abbr -a gitp git push
