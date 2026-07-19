83d101b6baaf21927682305a1c2a236dea99b853
William <fisetwill@gmail.com>
2026-07-18 21:36:19 -0700

    Update fish config to support EZA.

--- fish-shell/config.fish
+++ fish-shell/config.fish
@@ -2,12 +2,9 @@ set fish_greeting ''
 
 fish_add_path ~/.local/bin
 
+set -gx EDITOR vim
+
 # Command shortcuts
-
-abbr -a l ls -F
-abbr -a ll ls -lhF
-abbr -a la ls -ahF
-
 abbr -a c cd
 abbr -a .. cd ..
 abbr -a ... cd ../..
@@ -15,7 +12,7 @@ abbr -a .... cd ../../..
 abbr -a ..... cd ../../../..
 
 # Path shortcuts
-abbr -a drive cd "/Users/william/Library/CloudStorage/GoogleDrive-fisetwill@gmail.com/My Drive"
+abbr -a drive cd "/Users/william/Library/CloudStorage/GoogleDrive-fisetwill@gmail.com/My\ Drive"
 abbr -a github cd ~/Desktop/GitHub
 abbr -a desk cd ~/Desktop
 
@@ -24,3 +21,18 @@ abbr -a gita git add -A
 abbr -a gits git status
 abbr -a gitc git commit -m 
 abbr -a gitp git push
+
+# SSH
+abbr -a willmini ssh williamfiset@willmini.duckdns.org
+
+# Tool replacements
+abbr -a top btop
+
+# Replace ls with eza. A modern file listing system. 
+export EZA_COLORS="ur=0:uw=0:ux=0:ue=0:gr=0:gw=0:gx=0:tr=0:tw=0:tx=0:su=0"
+alias l='ls'
+alias ls='eza --icons --group-directories-first'
+alias la='ls -la'
+
+# opencode
+fish_add_path /Users/william/.opencode/bin
